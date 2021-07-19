3c26d5dbd01add894b091234f2683d8724728fdf
angelsalascalvo <34268023+angelsalascalvo@users.noreply.github.com>
2021-07-19 21:00:27 +0200

    Initial Commit

---
+++ Assets/Node.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Node : MonoBehaviour{
+
+	//VAR PUB
+	[SerializeField]
+	private List<Node> neighbors;
+
+	//VAR PRIV
+	private Node parent;
+	private float distance;
+	
+
+
+
+
+	//------------------------------------------------------------>
+	// GETs + SETs
+	//------------------------------------------------------------>
+	public void SetParent(Node parent) => this.parent = parent;
+	public void SetDistance(float distance) => this.distance = distance;
+	public void SetNeighbors(List<Node> neighbors) => this.neighbors = neighbors;
+	public Node GetParent() => parent;
+	public float GetDistance() => distance;
+	public List<Node> GetNeighbors() => neighbors;
+
+
+
+
+
+	//------------------------------------------------------------>
+	// DEBUG
+	//------------------------------------------------------------>
+#if UNITY_EDITOR
+
+
+	private LineRenderer line;
+
+	/// <summary>
+	/// Marcar o desmarcar un nodo como parte de la ruta
+	/// </summary>
+	/// <param name="status"></param>
+	public void IsWay(bool status) {
+		
+		GetComponent<SpriteRenderer>().color = Color.gray;
+		if (line == null)
+			line = gameObject.AddComponent<LineRenderer>();
+		line.enabled = false;
+
+		if (status) {
+			gameObject.GetComponent<SpriteRenderer>().color = Color.yellow;
+
+			line.enabled = true;
+			line.material = new Material(Shader.Find("Legacy Shaders/Particles/Alpha Blended Premultiply"));
+			line.startColor = Color.yellow;
+			line.endColor = Color.yellow;
+			line.startWidth = 0.1f;
+			line.endWidth = 0.1f;
+			line.positionCount = 2;
+			line.useWorldSpace = true;
+			line.SetPosition(0, transform.position);
+			line.SetPosition(1, parent.transform.position);
+
+		} 
+	}
+
+	//------------------------------------------------------------>
+
+	/// <summary>
+	/// Dibujar los gizmos en el editor
+	/// </summary>
+	void OnDrawGizmos(){
+
+			Handles.Label(transform.position, gameObject.name);
+
+			if (neighbors == null)
+				return;
+			foreach(var neighbor in neighbors)
+			{
+				if (neighbor != null) {
+					Vector2 auxOrigin = transform.position;
+					Vector2 auxTarget = neighbor.transform.position;
+
+
+					if (auxOrigin.y >= auxTarget.y) {
+						Gizmos.color = Color.blue;
+					} else {
+						Gizmos.color = Color.cyan;
+						auxTarget.x += 0.1f;
+						auxTarget.y += 0.1f;
+						auxOrigin.x += 0.1f;
+						auxOrigin.y += 0.1f;
+					}
+					Gizmos.DrawLine(auxOrigin, auxTarget);
+
+				}
+			}
+		}
+	#endif
+
+}
---
+++ Assets/PathManager.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathManager : MonoBehaviour{
+
+	//VAR PUB
+	public Transform targetPosition;
+
+	//VAR PRIV
+	private Node[] allNodes;
+
+	//------------------------------------------------------------>
+
+	/// <summary>
+	/// Ejecucion al iniciar el script
+	/// </summary>	
+	private void Start() {
+		//Obtener todos los nodos de la escena
+		GameObject[] auxAllNodes = GameObject.FindGameObjectsWithTag("Node");
+		allNodes = new Node[auxAllNodes.Length];
+		for (int i = 0; i < auxAllNodes.Length; i++) {
+			allNodes[i] = auxAllNodes[i].GetComponent<Node>();
+		}
+    }
+
+    //------------------------------------------------------------>
+
+    /// <summary>
+    /// Empleando el algoritmo A* se realiza la busqueda del path más optimo hasta llegar al nodo mas proximo a la posición de destino.
+    /// 
+    /// Funcionamiento:
+    /// Partiendo del analisis del nodo mas proximo a la posicion de origen se establecera para cada uno de sus nodos vecinos una puntuación basada en la distancia desde estos hasta el nodo destino,
+    /// de este modo se seleccionara el que tenga una puntuacion mas optima (mas cercano al objetivo) y pasara a analizarse este. Repitiendo este proceso continuamente finalmente se alcanzara el nodo objetivo o destino.
+    /// Una vez llegado al final se podra reconstruir el camino a seguir ya que cada nodo almacenara el padre desde el cual se ha accedido al mismo.
+    /// </summary>
+    /// <param name="destination"></param>
+    public Stack<Node> NavigateTo(Vector2 destination){
+
+		//1. Inicializacion
+		Stack<Node> path = new Stack<Node>();
+		Node currentNode = FindClosestNode(transform.position); //Inicialmente el nodo más cercano al inicio
+		Node endNode = FindClosestNode(destination);
+
+		//Verificar existencia de inicio, final y diferencia entre ambos
+		if (currentNode == null || endNode == null || currentNode == endNode)
+			return path;
+		
+		//Lista abierta, almacena nodos para analizar (busqueda del vecino mas optimo)
+		SortedList<float, Node> openList = new SortedList<float, Node>();
+		//Lista cerrada, almacena nodos ya analizados
+		List<Node> closedList = new List<Node> ();
+
+		//Parametros del primer nodo
+		openList.Add (0, currentNode);
+		currentNode.SetParent(null);
+		currentNode.SetDistance(0f);
+
+		//2. Analisis de nodos hasta llegar al final
+		while (openList.Count > 0){
+
+			//Obtener el de la lista ordenada el valor mas cercano
+			currentNode = openList.Values[0];
+			openList.RemoveAt (0);			
+			float dist = currentNode.GetDistance();
+			closedList.Add (currentNode);
+
+			//Finalizar al llegar al objetivo
+			if (currentNode == endNode)
+				break;
+			
+			//Recorrer nodos vecinos
+			foreach (Node neighbor in currentNode.GetNeighbors()){
+
+				//Ignorar analisis si ya han sido analizado (lista cerrada) o esta pendiente (lista abierta) 
+				if (closedList.Contains (neighbor) || openList.ContainsValue (neighbor))
+					continue;
+
+				//Almacenar el nodo vecino en la lista abierta ordenado por distancia
+				neighbor.SetParent(currentNode);
+				neighbor.SetDistance(dist + Vector2.Distance(neighbor.transform.position, currentNode.transform.position));
+				float distanceToTarget = Vector2.Distance(neighbor.transform.position, endNode.transform.position);
+				openList.Add (neighbor.GetDistance() + distanceToTarget, neighbor);
+			}
+		}
+
+		//3. Se ha encontrado el camino hasta el nodo final. 
+		if (currentNode == endNode){
+			//Recorrer hacia atrás los nodos seleccionados a traves de su parametro previus y formar la pila de recorrido a seguir
+			while (currentNode.GetParent() != null){
+				path.Push (currentNode);
+				currentNode = currentNode.GetParent();
+			}
+			//path.Push(endNode);
+		}
+
+		return path;
+	}
+
+	//------------------------------------------------------------>
+
+	/// <summary>
+	/// En el update se consigue que el objeto que tiene asociado este script de desplace
+	/// </summary>
+	void Update(){
+		foreach (Node node in allNodes) {
+			node.IsWay(false);
+		}
+
+		Stack<Node> stack = NavigateTo(targetPosition.position);
+		while (stack.Count > 0) {
+			Node node = stack.Pop();
+			node.IsWay(true);
+		}
+	}
+
+	//------------------------------------------------------------>
+
+	/// <summary>
+	/// Encuentra el waypoint más cercano respecto a la posicion pasada por parametro
+	/// </summary>
+	/// <param name="target"></param>
+	/// <returns></returns>
+	private Node FindClosestNode(Vector2 targetPosition){
+		Node closest = null;
+		float minDist = float.MaxValue;
+
+		for (int i = 0; i < allNodes.Length; i++) {
+			float dist = Vector2.Distance(allNodes[i].transform.position, targetPosition);
+			if (dist < minDist) {
+				minDist = dist;
+				closest = allNodes[i];
+			}
+		}
+
+		return closest;
+	}
+}
